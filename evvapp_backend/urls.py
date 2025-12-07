5eafbb4ad9843770fd6da1700bfd045f5c4f6f75
Clinton26 <134915423+Wanyangu1@users.noreply.github.com>
2025-12-07 21:42:19 +0300

    wip: latest changes

--- evvapp_backend/urls.py
+++ evvapp_backend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path("api/", include("evvapp_backend.api.urls")),
     path('api/evv/', include('evv.urls')),
 ]
